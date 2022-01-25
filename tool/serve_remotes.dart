55ea1cd11d1112049236d21efd7d9f8545389457
Pol Sieira <pol.sieira@workiva.com>
2022-01-25 10:59:15 -0800

    chore(package): downgrade http dep

--- tool/serve_remotes.dart
+++ tool/serve_remotes.dart
@@ -1,16 +1,21 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
-import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_static/shelf_static.dart';
+
+import 'cors_headers.dart';
 
 main() {
   // Serve remotes for ref tests.
-  final specFileHandler = createStaticHandler('test/JSON-Schema-Test-Suite/remotes');
-  var specFileHandlerWithCors = const Pipeline().addMiddleware(corsHeaders()).addHandler(specFileHandler);
+  final specFileHandler =
+      createStaticHandler('test/JSON-Schema-Test-Suite/remotes');
+  var specFileHandlerWithCors =
+      const Pipeline().addMiddleware(corsHeaders()).addHandler(specFileHandler);
   io.serve(specFileHandlerWithCors, 'localhost', 1234);
 
-  final additionalRemotesHandler = createStaticHandler('test/additional_remotes');
-  var additionalRemotesHandlerWithCors =
-      const Pipeline().addMiddleware(corsHeaders()).addHandler(additionalRemotesHandler);
+  final additionalRemotesHandler =
+      createStaticHandler('test/additional_remotes');
+  var additionalRemotesHandlerWithCors = const Pipeline()
+      .addMiddleware(corsHeaders())
+      .addHandler(additionalRemotesHandler);
   io.serve(additionalRemotesHandlerWithCors, 'localhost', 4321);
 }
