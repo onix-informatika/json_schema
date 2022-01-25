477b2d3f0af99b06365ff5289985d919408b99dd
Pol Sieira <pol.sieira@workiva.com>
2022-01-25 11:46:35 -0800

    chore: include copywright credit from shelf_cors_headers

--- tool/cors_headers.dart
+++ tool/cors_headers.dart
@@ -1,3 +1,10 @@
+// From dart package shelf_cors_headers (https://github.com/lenniezelk/shelf-cors-headers)
+//
+// Copyright © 2021 Leonard Kioi
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import 'package:shelf/shelf.dart';
 
 const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
@@ -19,7 +26,14 @@ const _defaultHeadersList = [
   'user-agent',
 ];
 
-const _defaultMethodsList = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT'];
+const _defaultMethodsList = [
+  'DELETE',
+  'GET',
+  'OPTIONS',
+  'PATCH',
+  'POST',
+  'PUT'
+];
 
 Map<String, String> _defaultHeaders = {
   ACCESS_CONTROL_EXPOSE_HEADERS: '',
@@ -29,14 +43,15 @@ Map<String, String> _defaultHeaders = {
   ACCESS_CONTROL_MAX_AGE: '86400',
 };
 
-final _defaultHeadersAll = _defaultHeaders.map((key, value) => MapEntry(key, [value]));
+final _defaultHeadersAll =
+    _defaultHeaders.map((key, value) => MapEntry(key, [value]));
 
 typedef OriginChecker = bool Function(String origin);
 
 bool originAllowAll(String origin) => true;
 
-OriginChecker originOneOf(List<String> origins) => (origin) => origins.contains(origin);
-
+OriginChecker originOneOf(List<String> origins) =>
+    (origin) => origins.contains(origin);
 Middleware corsHeaders({
   Map<String, String> headers,
   OriginChecker originChecker = originAllowAll,
