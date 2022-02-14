75dae003215d84ebf83676539ef3796ee4b6a460
Robbie Lamb <robbie.lamb@workiva.com>
2022-02-14 08:10:40 -0700

    Add in a Deprecated message.

--- lib/src/json_schema/json_schema.dart
+++ lib/src/json_schema/json_schema.dart
@@ -1776,6 +1776,7 @@ class JsonSchema {
   JsonSchema get unevaluatedItems => _unevaluatedItems;
 
   /// The set of functions to validate custom keywords.
+  @Deprecated("For use with the Validator only")
   Map<String, CustomValidationResult Function(Object)> get customAttributeValidators => _customAttributeValidators;
 
   // --------------------------------------------------------------------------
