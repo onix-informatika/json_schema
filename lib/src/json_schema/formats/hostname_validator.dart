b96dd0d68f22140733afa23fcec4ceb9f82f279f
Michael Carter <michael.carter@workiva.com>
2022-03-03 10:01:46 -0500

    fix(hostname-validator): change minimum version conditional

--- lib/src/json_schema/formats/hostname_validator.dart
+++ lib/src/json_schema/formats/hostname_validator.dart
@@ -3,7 +3,7 @@ import 'package:json_schema/src/json_schema/models/schema_version.dart';
 import 'package:json_schema/src/json_schema/models/validation_context.dart';
 
 ValidationContext defaultHostnameValidator(ValidationContext context, String instanceData) {
-  final regexp = context.schemaVersion > SchemaVersion.draft2019_09
+  final regexp = context.schemaVersion < SchemaVersion.draft2019_09
       ? JsonSchemaValidationRegexes.hostname
       // Updated in Draft 2019-09
       : JsonSchemaValidationRegexes.hostnameDraft2019;
